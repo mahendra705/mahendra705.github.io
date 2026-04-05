c25aaaf724080ae9fee5c056823ec66e9fd1ab0f
Mahendra Kumar Agrawal <manishagrawal705@gmail.com>
2026-04-05 03:41:25 +0000

    Updates

--- assets/Navbar-BTekn8A-.js
+++ assets/Navbar-BTekn8A-.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-C9ODASuc.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-Cd-3dyNw.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
