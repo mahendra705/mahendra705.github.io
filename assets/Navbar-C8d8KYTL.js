ed290688152e2bd00bb6851d9bd909353640f4a4
Mahendra Kumar Agrawal <manishagrawal705@gmail.com>
2026-04-08 18:46:22 +0000

    Updates

--- assets/Navbar-C8d8KYTL.js
+++ assets/Navbar-C8d8KYTL.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-CTl0WKKW.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-C2tHPJnC.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
